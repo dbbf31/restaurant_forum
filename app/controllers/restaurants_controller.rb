8a3cd22d49645806052da055baa363c48b2996b8
dbbf31 <doublebchen@gmail.com>
2018-01-13 23:04:42 +0800

    setup restaurant resources for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,12 +2,6 @@ class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
-    
+    @restaurants = Restaurant.page(params[:page]).per(9)
   end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-    @comment = Comment.new
-  end
-
 end
