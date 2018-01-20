34569887fb46ce832ac5da0e630b70ebec069786
dbbf31 <doublebchen@gmail.com>
2018-01-20 16:41:43 +0800

    user can add comment on restaurant show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113073320) do
+ActiveRecord::Schema.define(version: 20180120073652) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180113073320) do
     t.text "content"
     t.integer "restaurant_id"
     t.integer "user_id"
-    t.datetime "create_at", null: false
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
