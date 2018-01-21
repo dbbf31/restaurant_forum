864e6ef91ae1c54512414b9d9ad63ff45f2c45e3
dbbf31 <doublebchen@gmail.com>
2018-01-21 23:21:10 +0800

    fix create comment again

--- db/migrate/20180121150246_create_comments.rb
+++ db/migrate/20180121150246_create_comments.rb
@@ -1,6 +1,5 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
-    drop_table :comments, force: :cascade
     create_table :comments do |t|
       t.text :content
       t.integer :restaurant_id
