b62f0deb594811b1e2dbd361384777be3fe77d0b
dbbf31 <doublebchen@gmail.com>
2018-01-21 23:16:06 +0800

    fix create comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120174306) do
+ActiveRecord::Schema.define(version: 20180121150246) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
